9965fe6b797071030172c1734beb774981816fd0
garin <ekeqwert@gmail.com>
2025-09-20 13:22:45 +0300

    w

--- src/App.kt
+++ src/App.kt
@@ -1,35 +1,83 @@
-fun main()
-{
-    println("Введите первое число: ")
-    val number1 = readln().toInt()
-    println("Введите второе число: ")
-    val number2 = readln().toInt()
-    if (number1>number2){
-        println("Первое больш")
-    }
-    else if (number1== number2){
-        println("Равны")
-    }
-    else{
-        println("Второе больше")
-    }
-    val max = if(number1> number2) number1 else number2
+//fun main()
+//{
+//    println("Введите первое число: ")
+//    val number1 = readln().toInt()
+//    println("Введите второе число: ")
+//    val number2 = readln().toInt()
+//    if (number1>number2){
+//        println("Первое больш")
+//    }
+//    else if (number1== number2){
+//        println("Равны")
+//    }
+//    else{
+//        println("Второе больше")
+//    }
+//    val max = if(number1> number2) number1 else number2
+//
+//    val month = readln().toInt()
+//    if (month in 1..12) {
+//        println("Ваш месяц под номером $month соответствует месяцу ")
+//    }
+//    if (month == 1) println("Январь")
+//    else if (month == 2) println("Февраль")
+//    else if (month == 3) println("Март")
+//    else if (month == 4) println("Апрель")
+//    else if (month == 5) println("Май")
+//    else if (month == 6) println("Июнь")
+//    else if (month == 7) println("Июль")
+//    else if (month == 8) println("Август")
+//    else if (month == 9) println("Сентябрь")
+//    else if (month == 10) println("Октябрь")
+//    else if (month == 11) println("Ноябрь")
+//    else if (month == 12) println("Декабрь")
+//    println(max)
+//}
 
-    val month = readln().toInt()
-    if (month in 1..12) {
-        println("Ваш месяц под номером $month соответствует месяцу ")
-    }
-    if (month == 1) println("Январь")
-    else if (month == 2) println("Февраль")
-    else if (month == 3) println("Март")
-    else if (month == 4) println("Апрель")
-    else if (month == 5) println("Май")
-    else if (month == 6) println("Июнь")
-    else if (month == 7) println("Июль")
-    else if (month == 8) println("Август")
-    else if (month == 9) println("Сентябрь")
-    else if (month == 10) println("Октябрь")
-    else if (month == 11) println("Ноябрь")
-    else if (month == 12) println("Декабрь")
-    println(max)
-}
+// LABA NO 2
+
+// шаг 6
+//fun main() {
+//    val level = readln().toInt()
+//    if (level < 10){
+//        println("Новичок! Вы получаете +10HP")
+//    }
+//    else if (10 <= level and level<50){
+//
+//    }
+//}
+
+//шаг 4
+//fun main() {
+//    print("Ваш уровень: ")
+//    val level = readln().toInt()
+//
+//    print("У вас есть Випка?(t/f):")
+//    val vip = readln().toBoolean()
+//
+//    if ((level >= 30 && vip) || (level >= 50)) {
+//        println("Доступ разрешен")
+//    }
+//    else {
+//        println("Доступ запрещен")
+//    }
+//}
+
+////шаг 5
+//fun main() {
+//    print("Ваш уровень: ")
+//    val level = readln().toInt()
+//    if (level in 5 until 11) {
+//        println("Уровень $level подходит под условие. Вход разрешен!")
+//    } else {
+//        println("Уровень высок. Вход запрещен!")
+//    }
+//    if (level in 1..10 step 3) {
+//        println("Уровень $level подходит под условие. Вход разрешен!")
+//    } else {
+//        println("Уровень высок. Вход запрещен!")
+//    }
+//
+//}
+
+//зад 1
